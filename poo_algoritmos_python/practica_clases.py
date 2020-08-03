99c3d17722a82607f0847864a75acc565d038315
pedroromn <pedroromn@gmail.com>
2020-08-03 11:40:10 -0500

    03/08/2020 11:40

---
+++ poo_algoritmos_python/practica_clases.py
@@ -0,0 +1,13 @@
+Python 3.7.7 (tags/v3.7.7:d7c567b08f, Mar 10 2020, 10:41:24) [MSC v.1900 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> class Persona:
+	def __init__(self):
+		pass
+
+	
+>>> p = Persona()
+>>> p.__class__
+<class '__main__.Persona'>
+>>> type(p)
+<class '__main__.Persona'>
+>>> 
