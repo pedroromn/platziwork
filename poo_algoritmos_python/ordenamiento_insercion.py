53edf5050a49cfbcc1ef890c9d7aa9a678ed778d
pedroromn <localhost@domain.com>
2020-08-03 02:48:48 -0500

    03/08/2020 02:48

--- poo_algoritmos_python/ordenamiento_insercion.py
+++ poo_algoritmos_python/ordenamiento_insercion.py
@@ -5,13 +5,25 @@ import random
 def ordenamiento_insercion(lista):
     for indice in range(1, len(lista)):
         valor_actual = lista[indice]
+        print('+'*50)
+        print("Inicio del ciclo for")
+        print(f"valor_actual <= lista[indice] : valor_actual <= {lista[indice]}")
         posicion_actual = indice
+        print(f"posicion_actual <= indice : posicion_actual <= {indice}")
         
         while posicion_actual > 0 and lista[posicion_actual-1] > valor_actual:
+            print('+'*50)
+            print("Dentro del ciclo while")
             lista[posicion_actual] = lista[posicion_actual-1]
+            print('+'*50)
+            print(f"lista[posicion_actual] <= lista[posicion_actual-1] : lista[{posicion_actual}] <= {lista[posicion_actual-1]}")
             posicion_actual -= 1
+            print(f"posicion_actual -= 1 : posicion_actual: {posicion_actual}")
             
         lista[posicion_actual] = valor_actual
+        print('+'*50)
+        print("Fuera del ciclo while")
+        print(f"lista[posicion_actual] <= valor_actual : lista[{posicion_actual}] <= {valor_actual}")
         
     return lista
         
